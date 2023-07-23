29ad24723b9fb4eb5667f598df6cf546caaef82a
r-4bb1t <r4bb1t.github@gmail.com>
2023-07-23 23:49:34 +0900

    feat: add input component

---
+++ src/components/common/Input.jsx
@@ -0,0 +1,18 @@
+import cc from "classcat";
+
+export default function Input({ label, ...props }) {
+  return (
+    <div className="form-control w-full max-w-xs">
+      <label className="text-primary text-xs mb-1 px-1 font-medium">
+        {label}
+      </label>
+      <input
+        {...props}
+        className={cc([
+          "input input-primary input-bordered w-full",
+          props.className,
+        ])}
+      />
+    </div>
+  );
+}
