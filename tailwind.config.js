fd15e9742c9e7deaf1d340b9b701f81df502d819
r-4bb1t <r4bb1t.github@gmail.com>
2023-07-23 15:08:16 +0900

    style: add color system

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#d76e75",
+        secondary: {
+          light: "#feebdc",
+          DEFAULT: "#e5a1a0",
+        },
+        error: "#ff6b00",
+        black: {
+          200: "#f6f7f9",
+          400: "#dddddd",
+          600: "#676767",
+          800: "#444444",
+          DEFAULT: "#000000",
+        },
+        White: "#ffffff",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
