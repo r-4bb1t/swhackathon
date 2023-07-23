3f66e53fc2f0f0440d5e22f106d71650b2d6cb35
r-4bb1t <r4bb1t.github@gmail.com>
2023-07-23 21:57:56 +0900

    feat: add icons

---
+++ src/components/Icons/index.jsx
@@ -0,0 +1,7 @@
+import Logo from "./logo.svg";
+import LeftChevron from "./leftChevron.svg";
+import RightChevron from "./rightChevron.svg";
+import LeftArrow from "./leftArrow.svg";
+import RightArrow from "./rightArrow.svg";
+
+export default { Logo, LeftChevron, RightChevron, LeftArrow, RightArrow };
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  plugins: [react(), svgr({ exportAsDefault: true })],
 });
