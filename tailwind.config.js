ec73807a6f77cabe33a71b791a35a2917a9b0371
r-4bb1t <r4bb1t.github@gmail.com>
2023-07-23 15:14:10 +0900

    style: add font size system

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,61 @@ export default {
         White: "#ffffff",
       },
     },
+    fontSize: {
+      h1: {
+        fontSize: "22px",
+        lineHeight: "34px",
+        fontWeight: "700",
+      },
+      h2: {
+        fontSize: "20px",
+        fontWeight: "600",
+      },
+      subtitle: {
+        lg: {
+          fontSize: "18px",
+          lineHeight: "28px",
+          fontWeight: "600",
+        },
+        bold: {
+          fontSize: "16px",
+          lineHeight: "24px",
+          fontWeight: "600",
+        },
+        DEFAULT: {
+          fontSize: "16px",
+          lineHeight: "22px",
+          fontWeight: "400",
+        },
+      },
+      body: {
+        lg: {
+          fontSize: "17px",
+          fontWeight: "400",
+        },
+        bold: {
+          fontSize: "17px",
+          fontWeight: "700",
+        },
+        DEFAULT: {
+          fontSize: "14px",
+          lineHeight: "20px",
+          fontWeight: "400",
+        },
+      },
+      caption: {
+        sm: {
+          fontSize: "10px",
+          lineHeight: "16px",
+          fontWeight: "400",
+        },
+        DEFAULT: {
+          fontSize: "12px",
+          lineHeight: "16px",
+          fontWeight: "600",
+        },
+      },
+    },
     plugins: [],
   },
 };
