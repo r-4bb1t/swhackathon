cf5163a96ffb9ad99a3be1385a14c76a2e2ba601
r-4bb1t <r4bb1t.github@gmail.com>
2023-07-31 01:57:19 +0900

    feat: add userType to user

--- src/recoil/atoms/userState.jsx
+++ src/recoil/atoms/userState.jsx
@@ -2,11 +2,11 @@ import { atom } from "recoil";
 // user 관련 atoms 예시
 
 export const userInfoState = atom({
-    // 유저 정보
-    key: "userInfoState",
-    default: {
-        id: "",
-        name: "",
-        isLogin: false,
-    },
+  key: "userInfoState",
+  default: {
+    id: "",
+    name: "",
+    userType: "",
+    isLogin: false,
+  },
 });
