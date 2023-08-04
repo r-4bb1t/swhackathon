f8729556df9f44863876f649c2aaf39f3ceb160f
Dawon00 <dawon000203@gmail.com>
2023-08-04 20:53:40 +0900

    fix: add id to userInfoState

--- src/recoil/atoms/userState.jsx
+++ src/recoil/atoms/userState.jsx
@@ -2,9 +2,10 @@ import { atom } from "recoil";
 import { childrenBirthSelector } from "../selectors/childrenState";
 
 export const userInfoState = atom({
-    // 부모 유저 정보
+    // 유저 정보
     key: "userInfoState",
     default: {
+        id: "",
         introduction: "",
         name: "",
         password: "",
